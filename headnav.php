9780603e2d3b173b0f16036383a099783dbecaea
AkiraReiDeveloper <45138556+AkiraReiDeveloper@users.noreply.github.com>
2019-08-30 14:27:15 -0600

    Add files via upload

---
+++ headnav.php
@@ -0,0 +1,27 @@
+<header>
+
+	<div >
+		<div class="box">
+		    <div class="container-1">
+		      	<span ><i class=""></i></span>
+		      	<input type="search" id="search" placeholder="Que deceas comprar..." />
+		    </div>
+		</div>
+		<nav >
+			<ul>
+				<li></li>
+
+				<ul >
+					<li><a href=""><i>Categorias</i></a></li>
+					<li><a href=""><i>Descuentos del dia</i></a></li>
+					<li><a href=""><i>Ayuda</i></a></li>
+				</ul>
+				<ul >
+					<li><a href="" ><i>Crear Cuenta</i></a></li>
+					<li><a href="" ><i>Ingresar</i></a></li>
+					<li><a href=""></a></li>
+				</ul>
+			</ul>
+		</nav>
+	</div>
+</header>
