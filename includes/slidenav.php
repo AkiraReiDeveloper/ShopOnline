b374891d47ee7e21dddabb7fbd5ea441eccb8bdc
multiplataforma7 <multiplataforma7@Mac-mini-de-multiplataforma7.local>
2019-09-17 19:32:25 -0600

    adaptacion del idex

---
+++ includes/slidenav.php
@@ -0,0 +1,16 @@
+<div class="col-lg-3">
+
+  <h1 class="lead">Categorias</h1>
+  <div class="list-group">
+    <?php
+      $conexion = ConexionDB::conexion();
+      $sql
+
+     ?>
+    <a href="#" class="list-group-item">Category 1</a>
+    <a href="#" class="list-group-item">Category 2</a>
+    <a href="#" class="list-group-item">Category 3</a>
+  </div>
+
+</div>
+<!-- /.col-lg-3 -->
