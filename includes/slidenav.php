23603eb96f3991d9583f57dda45724ff19d0c47d
multiplataforma7 <multiplataforma7@Mac-mini-de-multiplataforma7.local>
2019-09-18 19:25:18 -0600

    modificacion del sidenava para cargar la iformacion directa de la base de datos

--- includes/slidenav.php
+++ includes/slidenav.php
@@ -4,12 +4,24 @@
   <div class="list-group">
     <?php
       $conexion = ConexionDB::conexion();
-      $sql
+      $sql = "select * from categorias";
+      $resultado = $conexion->prepare($sql);
+      if(!$resultado->execute()){
+        echo"<h1 style='color:red'></h1>";
+      }else{
+        while($registro = $resultado->fetch()){
+          $id_categoria = $registro["id_categoria"];
+          $titulo_categoria = $registro["nombre"];
+          echo "<a href='categoria/categoria.php?
+          id_categoria=$id_categoria'class='list-group-item'>$titulo_categoria</a>";
+        }
+      }
 
      ?>
+     <!--
     <a href="#" class="list-group-item">Category 1</a>
     <a href="#" class="list-group-item">Category 2</a>
-    <a href="#" class="list-group-item">Category 3</a>
+    <a href="#" class="list-group-item">Category 3</a>-->
   </div>
 
 </div>
