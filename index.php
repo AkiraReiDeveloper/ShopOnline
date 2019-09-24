4d5d8fe6ba9d679202d7a1e5a0d4ea466f86c969
multiplataforma7 <multiplataforma7@Mac-mini-de-multiplataforma7.local>
2019-09-23 19:30:39 -0600

    modificacion del index para cargar los productos directamente de la base de datos

--- index.php
+++ index.php
@@ -18,103 +18,43 @@
            ?>
 
         <div class="row">
+          <?php
+            $conexion = ConexionDB::conexion();
+            $sql="select * from productos";
+            $resultado = $conexion->prepare($sql);
+            if(!$resultado->execute()){
+              echo"<h1 style='color:red'></h1>";
+            }else{
+              while($registro = $resultado->fetch()){
+                $id_producto = $registro["id_productos"];
+                $nombre_producto = $registro["nombre"];
+                $precio_producto = $registro["precio"];
+                $image_producto = $registro["img"];
+                $descripcion_corta_producto = $registro["descripcion-corta"];
+                $descripcion_producto = $registro["descripcion"];
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="Templates/Producto/">Item One</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
+           ?>
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="/Templates/Producto"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="Templates/Producto/">Item Two</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="Templates/Producto/">Item Three</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="Templates/Producto/">Item Four</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="Templates/Producto/">Item Five</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="Templates/Producto/">Item Six</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
+           <div class="col-lg-4 col-md-6 mb-4">
+             <div class="card h-100">
+               <a href="producto.php?id_producto=<?php echo $id_producto;?>"><img class="card-img-top" src="<?php echo $image_producto;?>" alt=""></a>
+               <div class="card-body">
+                 <h4 class="card-title">
+                   <a href="producto.php?id_producto=<?php echo $id_producto?>"><?php echo $nombre_producto;?></a>
+                 </h4>
+                 <h5>&#36;<?php echo $precio_producto;?></h5>
+                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                 <a href="producto.php?id_producto=<?php echo $id_producto;?>" class="btn btn-primary" target="_blank" > Agregar al Carro</a>
+               </div>
+               <div class="card-footer">
+                 <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+               </div>
+             </div>
+           </div>
+           <?php
+         }
+       }
+          ?>
         </div>
         <!-- /.row -->
 
